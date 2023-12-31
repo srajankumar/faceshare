900efe4a697fc13527ad5efc1ead304a4f6b172a
srajan <kumarsrajan02@gmail.com>
2023-12-31 17:01:44 +0530

    ui changes

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -19,19 +19,15 @@ export default function Login() {
           <img
             src="/login.jpg"
             className="relative bg-black object-cover w-full h-full z-20"
-            // layout="fill"
             alt=""
           ></img>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex flex-col justify-center space-y-6 w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Login to continue
               </h1>
-              {/* <p className="text-sm text-muted-foreground">
-                Enter your credentials below to create your account
-              </p> */}
             </div>
             <UserAuthForm />
             <p className="px-8 text-center text-sm text-muted-foreground">
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -24,7 +24,7 @@ export default function Login() {
           ></img>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="mx-auto flex flex-col justify-center space-y-6 w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Create an account
