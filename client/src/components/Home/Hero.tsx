ba94ff2aa1d0d3084079ca363df9ba5d7cbfb607
srajan <kumarsrajan02@gmail.com>
2023-12-31 09:34:29 +0530

    ui fix

--- client/src/components/Home/Hero.tsx
+++ client/src/components/Home/Hero.tsx
@@ -9,15 +9,15 @@ const Hero = () => {
         </div>
       </div> */}
         <div className="text-center">
-          <h1 className="text-4xl sm:leading-[4.5rem] sm:text-6xl font-semibold">
+          <h1 className="text-4xl sm:leading-[3.5rem] sm:text-5xl font-semibold">
             The Open Source Linktree Substitute
           </h1>
-          <p className="md:mt-6 mt-4 md:text-lg text-sm md:leading-8 leading-6 text-primary/50">
+          <p className="md:mt-6 mt-4 md:text-lg md:leading-8 leading-6 text-primary/50">
             Build your awesome profile and easily share it with your connections
             for free! Create a standout online presence that reflects your
             uniqueness.
           </p>
-          <div className="md:mt-10 md:text-base text-sm mt-5 flex items-center justify-center gap-x-6">
+          <div className="md:mt-10 mt-5 flex items-center justify-center gap-x-6">
             <Link
               href="/login"
               className="rounded-full bg-gradient-to-r from-[#8ebec0] to-[#f8914c] px-5 py-2.5 font-bold shadow-sm text-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
