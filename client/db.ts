79ef5d9a0c79cc60e1359c171998d4cde2f20b84
srajan <kumarsrajan02@gmail.com>
2024-04-09 16:08:47 +0530

    hmm

--- client/db.ts
+++ client/db.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
+const uri: string = process.env.NEXT_PUBLIC_MONGODB_URI || "";
+
 const connect = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://srajankumar:zoro123@cluster0.ixjwkbo.mongodb.net/profiles?retryWrites=true&w=majority"
-    );
+    await mongoose.connect(uri);
     console.log("mongodb connected successfully.");
   } catch (err) {
     throw new Error("Error connecting to mongodb.");
